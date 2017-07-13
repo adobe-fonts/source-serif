27fe6141147049ba85590b369af4a3799dea002e
Miguel Sousa <msousa@adobe.com>
2017-07-13 05:25:32 -0700

    Move features.fea files inside the UFOs

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/env sh
 
 family=SourceSerifPro
 weights='ExtraLight Light Regular Semibold Bold Black'
@@ -9,7 +9,7 @@ mkdir target/ target/OTF/ target/TTF/
 
 for w in $weights
 do
-  makeotf -f Roman/$w/font.ufo -r -o target/OTF/$family-$w.otf
-  makeotf -f Roman/$w/font.ttf -r -o target/TTF/$family-$w.ttf
-  rm Roman/$w/current.fpr # remove default options file from the source tree after building
+  font_path=Roman/$w/font
+  makeotf -f $font_path.ufo -r -o target/OTF/$family-$w.otf
+  makeotf -f $font_path.ttf -r -o target/TTF/$family-$w.ttf -ff $font_path.ufo/features.fea
 done
