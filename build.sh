ef6718ec1be3f47e1902ec363c02f3d693e653d1
Frank Grießhammer <fgriessh@adobe.com>
2018-08-07 11:30:57 -0700

    Refine build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 family=SourceSerifPro
 roman_weights='ExtraLight Light Regular Semibold Bold Black'
 italic_weights='ExtraLightIt LightIt It SemiboldIt BoldIt BlackIt'
-fonttools_dir=~/code/fonttoolse
+fonttools_dir=~/code/fonttools
 comp_script=$fonttools_dir/Snippets/otf2ttf.py
 
 script_found=True
@@ -30,9 +30,11 @@ do
   font_dir=Roman/Instances/$w
   font_ufo=$font_dir/font.ufo
   ps_name=$family-$w
-
+  echo $ps_name
   echo "Building OTF ..."
-  makeotf -f $font_ufo -r -o $font_dir/$ps_name.otf
+  # -r is for "release mode" (subroutinization + applied glyph order)
+  # -gs is for filtering the output font to contain only glyphs in the GOADB
+  makeotf -f $font_ufo -r -gs -o $font_dir/$ps_name.otf
   echo "Building TTF ..."
   python3 $comp_script $font_dir/$ps_name.otf -o $font_dir/$ps_name.ttf
   echo "Componentizing TTF ..."
@@ -54,7 +56,7 @@ do
   ps_name=$family-$w
 
   echo "Building OTF ..."
-  makeotf -f $font_ufo -r -o $font_dir/$ps_name.otf
+  makeotf -f $font_ufo -r -gs -o $font_dir/$ps_name.otf
   echo "Building TTF ..."
   python3 $comp_script $font_dir/$ps_name.otf -o $font_dir/$ps_name.ttf
   echo "Componentizing TTF ..."
