824cccc1a0630cd2095b0205af75ca877ec9fc4a
Frank Grießhammer <fgriessh@adobe.com>
2022-08-31 12:50:46 +0200

    Remove “comment out STAT.fea” hack

--- buildVFs.sh
+++ buildVFs.sh
@@ -30,20 +30,15 @@ function build_var_font {
 	# replace CFF2 table with subroutinized version
 	sfntedit -a CFF2=$1/.tb_cff2 $1/$2.otf
 
-	# comment out STAT feature file which cannot be digested by fontmake
-	sed -i '' 's/^/#/' $1/../STAT.fea
-	# build variable TTF with fontmake. Use set +e so we can un-hack the
-	# STAT.fea before exiting.
-	set +e
+	# build variable TTF with fontmake.
 	fontmake -m $1/$2.designspace -o variable --production-names --output-path $1/$2.ttf --feature-writer None
 	fm_status=$?
-	# undo changes to STAT feature file
-	sed -i '' 's/#//' $1/../STAT.fea
+
 	if [ $fm_status -ne 0 ]
 	then
 		exit $fm_status
 	fi
-	set -e
+
 	# use DSIG, name, OS/2, hhea, post, and STAT tables from OTFs
 	sfntedit -x DSIG=$1/.tb_DSIG,name=$1/.tb_name,OS/2=$1/.tb_os2,hhea=$1/.tb_hhea,post=$1/.tb_post,STAT=$1/.tb_STAT,fvar=$1/.tb_fvar $1/$2.otf
 	sfntedit -a DSIG=$1/.tb_DSIG,name=$1/.tb_name,OS/2=$1/.tb_os2,hhea=$1/.tb_hhea,post=$1/.tb_post,STAT=$1/.tb_STAT,fvar=$1/.tb_fvar $1/$2.ttf
