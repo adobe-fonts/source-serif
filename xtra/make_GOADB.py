631b21ec0794357a377836ef6a93dd3e2bbd7941
Frank Grießhammer <fgriessh@adobe.com>
2024-07-15 14:47:18 +0200

    implement automated GlyphOrderAndAliasDB building

---
+++ xtra/make_GOADB.py
@@ -0,0 +1,41 @@
+'''
+Takes no arguments, expects a masterGOADB file in repo root folder.
+Distributes GlyphOrderAndAliasDB files into Roman/Italic subfolders.
+'''
+
+import re
+from pathlib import Path
+
+
+def run():
+    italic_tag = '#-[italic]'
+    roman_tag = '#-[roman]'
+    script_name = Path(__file__).name
+    edit_warning = (
+        '# Please do not edit this GOADB. This file is built from a master GOADB,\n'
+        f'# found in the _xtra folder of this repository, using {script_name}.\n'
+    )
+
+    global_GOADB_path = Path(__file__).parent / 'masterGOADB'
+    with open(global_GOADB_path, 'r') as goadb_blob:
+        raw_mapping = goadb_blob.read().splitlines()
+
+    raw_roman_GOADB = [
+        line for line in raw_mapping if not line.startswith(italic_tag)]
+    raw_italic_GOADB = [
+        line for line in raw_mapping if not line.startswith(roman_tag)]
+
+    roman_mapping = [edit_warning] + [re.sub(
+        rf'{re.escape(roman_tag)} ?', '', line) for line in raw_roman_GOADB]
+    italic_mapping = [edit_warning] + [re.sub(
+        rf'{re.escape(italic_tag)} ?', '', line) for line in raw_italic_GOADB]
+
+    with open('../Roman/GlyphOrderAndAliasDB', 'w') as f:
+        f.write('\n'.join(roman_mapping) + '\n')
+
+    with open('../Italic/GlyphOrderAndAliasDB', 'w') as f:
+        f.write('\n'.join(italic_mapping) + '\n')
+
+
+if __name__ == '__main__':
+    run()
